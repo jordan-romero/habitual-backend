f7bb7a7d952c7f3846891f463272464d90e35f67
jordles113 <jordan.t.romero@gmail.com>
2020-11-30 14:22:47 -0600

    adds create action for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,23 @@ class Api::V1::UsersController < ApplicationController
         users = User.all 
         render json: users, status: 200
     end 
+
+    def create
+        user = User.new(user_params)
+        if user.save
+          session[:user_id] = user.id
+          render json: user, status: 200
+        else
+          response = {
+            error: user.errors.full_messages.to_sentence
+          }
+          render json: response, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    
+    def user_params
+        params.permit(:name, :username, :email, :password, :location)
+    end
 end
