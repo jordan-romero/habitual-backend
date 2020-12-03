2a560f58038715d8b31101372ad56d2a9219637a
jordles113 <jordan.t.romero@gmail.com>
2020-12-03 14:39:07 -0600

    builds out habits controller

--- app/controllers/api/v1/habits_controller.rb
+++ app/controllers/api/v1/habits_controller.rb
@@ -1,8 +1,48 @@
 class Api::V1::HabitsController < ApplicationController
+    before_action :set_habit, only: [:update, :destroy]
 
     def index 
         habits = current_user.habits 
         render json: habits, status: 200
     end 
 
+    def create 
+        habit = current_user.habits.build(habit_params)
+
+        if habit.save 
+            render json: habit, status: 200
+          else
+            error_resp = {
+              error: habit.errors.full_messages.to_sentence
+            }
+            render json: error_resp, status: :unprocessable_entity
+          end
+    end 
+
+    def update
+      if @habit.update(habit_params)
+          render json: @habit, status: 200
+      else
+        error_resp = {
+          error: @habit.errors.full_messages.to_sentence
+        }
+        render json: error_resp, status: :unprocessable_entity
+      end
+    end 
+
+    def destroy
+      @habit.destroy
+      render json: {notice: 'habit destroyed'}, status: 200
+    end
+
+
+    private 
+
+    def set_habit
+      @habit = habit.find_by(id: params[:id])
+    end 
+
+    def habit_params
+        params.require(:habit).permit(:name)
+    end 
 end
