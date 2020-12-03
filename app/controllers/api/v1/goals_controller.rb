91c4bf32af964e7a8b0a33af66dde36ec7434e56
jordles113 <jordan.t.romero@gmail.com>
2020-12-03 12:44:26 -0600

    adds create to goals controller

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -5,4 +5,23 @@ class Api::V1::GoalsController < ApplicationController
         render json: goals, status: 200
     end 
 
+    def create 
+        goal = current_user.goals.build(goal_params)
+
+        if goal.save 
+            render json: goal, status: 200
+          else
+            error_resp = {
+              error: goal.errors.full_messages.to_sentence
+            }
+            render json: error_resp, status: :unprocessable_entity
+          end
+    end 
+
+
+    private 
+
+    def goal_params
+        params.require(:goal).permit(:name)
+    end 
 end
