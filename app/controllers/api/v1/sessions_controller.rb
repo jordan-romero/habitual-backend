b6017b3e18c8f0bf0c5684e180d43bb9e6a365c0
jordles113 <jordan.t.romero@gmail.com>
2020-12-14 17:51:47 -0600

    cleans up sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create 
-        # byebug
         user = User.find_by(username: params[:session][:username])
     
         if user && user.authenticate(params[:session][:password])
