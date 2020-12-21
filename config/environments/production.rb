2d74f48249412559f04e2b8ac5fa77e65b76d3f6
jordles113 <jordan.t.romero@gmail.com>
2020-12-21 11:04:27 -0600

    testing:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'https://habitualbackend.herokuapp.com/api/v1/goals' }
+  config.action_mailer.default_url_options = { :host => 'https://habitualbackend.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
